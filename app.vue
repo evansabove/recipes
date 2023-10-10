1a45a2b97474b2441669cf64db2964eea2fdab62
Andy <andy.evans@razor.co.uk>
2023-10-10 15:27:09 +0100

    Basic Storyblok integration working

--- app.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <p>Hello</p>
-    <!-- Look here for examples https://stackblitz.com/edit/nuxt-3-sdk-demo?file=storyblok%2FFeature.vue -->
-  </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: process.env.STORYBLOK_KEY
+      },
+    ],
+    '@pinia/nuxt',
+  ],
+  devServer: {
+    https: {
+      key: 'localhost-key.pem',
+      cert: 'localhost.pem'
+    }
+  },
 })
