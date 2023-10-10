8d6f8b172d2f2c440ca237d430fb840e3d5f43c0
Andy <andy.evans@razor.co.uk>
2023-10-10 16:14:36 +0100

    Starting a layout

--- components/bloks/Page.vue
+++ components/bloks/Page.vue
@@ -4,26 +4,35 @@ import { PageStoryblok } from "../../types/component-types-sb";
 import { StoryblokTypes } from "../../types/StoryblokTypes";
 
 const props = defineProps({
-    story: {
-        type: Object as PropType<StoryblokTypes.Story<PageStoryblok>>,
-        required: true,
-    },
-    theme: {
-        type: Object,
-        required: false,
-    },
+  story: {
+    type: Object as PropType<StoryblokTypes.Story<PageStoryblok>>,
+    required: true,
+  },
+  theme: {
+    type: Object,
+    required: false,
+  },
 });
 </script>
 
 <template>
-    <article v-if="story.content?.body">
+  <UContainer>
+    <div class="flex flex-col">
+      <p class="text-3xl mb-5 flex-none">Site title</p>
+      <article v-if="story.content?.body" class="flex-1">
         <component
-            :is="blok.component"
-            v-for="blok in story.content.body"
-            :id="blok._id"
-            :key="blok._id"
-            v-editable="blok"
-            :blok="blok"
-            :story="story" />
-    </article>
+          :is="blok.component"
+          v-for="blok in story.content.body"
+          :id="blok._id"
+          :key="blok._id"
+          v-editable="blok"
+          :blok="blok"
+          :story="story"
+        />
+      </article>
+      <div class="flex-none">
+        <p>Footer</p>
+      </div>
+    </div>
+  </UContainer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       },
     ],
     '@pinia/nuxt',
-    '@nuxtjs/tailwindcss',
+    '@nuxt/ui'
   ],
   devServer: {
     https: {
